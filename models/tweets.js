da1602a6460af2038ed862ff59cc0b6483dfdb18
Your name <Your email>
2024-05-02 15:23:25 +0200

    commit router get post delete tweet

--- models/tweets.js
+++ models/tweets.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const tweetSchema = mongoose.Schema({
   date: Date,
   description: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  user: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
 });
 
 const Tweet = mongoose.model("tweets", tweetSchema);
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,25 +1,49 @@
 var express = require("express");
-const Tweet = require("../models/tweets");
 var router = express.Router();
-require("./models/connection");
-
-const { checkBody } = require("../modules/checkBody");
-
-router.post("/tweetuser", (req, res) => {
-  if (!checkBody(req, body, ["description"])) {
-    res.json({ result: false, error: "Missing or empty fields" });
-    return;
-  } else {
+const Tweet = require("../models/tweets");
+//router post ma donnée
+router.post("/tweetUser", (req, res) => {
+  //console.log(req.body.description);
+  if (req.body.description !== "") {
     Tweet.findOne({ description: req.body.description }).then((data) => {
       const newTweet = new Tweet({
         user: req.body.userId,
         description: req.body.description,
-        date: Date,
+        date: new Date(),
       });
 
-      newTweet.save().then((newDoc) => {
-        res.json({ result: true, tweet: newDoc });
+      newTweet.save().then(() => {
+        res.json({ result: true, tweet: newTweet });
       });
     });
+  } else {
+    res.json({ result: false });
   }
 });
+
+//router get recupere les tweet present dans ma database
+router.get("/gettweets", (req, res) => {
+  Tweet.find().then((data) => {
+    console.log(data);
+    if (!data) {
+      res.json({ result: false });
+    } else {
+      res.json({ result: data });
+    }
+  });
+});
+
+// router delete renvoie une foi suprimé les autres tweet
+router.delete("/:tweetId", (req, res) => {
+  Tweet.deleteOne({ _id: req.params.tweetId }).then((deletedDoc) => {
+    if (deletedDoc.deletedCount > 0) {
+      // document successfully deleted
+      Tweet.find().then((data) => {
+        res.json({ result: true, Tweet: data });
+      });
+    } else {
+      res.json({ result: false, error: "Tweet not found" });
+    }
+  });
+});
+module.exports = router;
