a818693d656e6ed06b4ccaeb651cf0f6effe74bb
Your name <Your email>
2024-05-02 13:07:01 +0200

    commit ninafrontend 1

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const cors = require('cors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cors = require("cors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const tweetRouter = require("./routes/tweet");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use(cors());
+app.use("/tweet", tweetRouter);
 
 module.exports = app;
---
+++ models/tweets.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = mongoose.Schema({
+  date: Date,
+  description: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+});
+
+const Tweet = mongoose.model("tweets", tweetSchema);
+
+module.exports = Tweet;
