9330124539c0f51a9d5e119c816d207ef5bfa60a
Wassim.rb <rbilawassim@gmail.coml>
2024-05-02 10:02:33 +0200

    push back

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(cors());
 
 module.exports = app;
